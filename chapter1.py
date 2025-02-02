600e0079a6ff45dccb893474ec76f81a49ea8a8c
Muxitdinov Husanjon <muxitdinov_h@internet.ahi>
2025-02-02 11:19:51 +0500

    first CV2 project

--- chapter1.py
+++ chapter1.py
@@ -1,11 +1,18 @@
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 
 #################   RASM o'qish  ############################
-# img = cv2.imread("Resources/news_i1.jpeg")
-#
-# cv2.imshow("Output", img)
-# cv2.waitKey(0)
+img = cv2.imread("Resources/news_i1.jpeg")
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+cv2.imshow("Output", img)
+cv2.waitKey(0)
+
+plt.figure()
+plt.imshow(img_rgb)
+# plt.imshow(img)
+plt.show()
 
 
 ################## Video o'qish   ##########################
@@ -23,16 +30,16 @@ import numpy as np
 
 
 #########################    READ WEBCAM  ###########################
-frameWidth = 640
-frameHeight = 480
-video = cv2.VideoCapture(0)
-video.set(3,frameWidth)
-video.set(4,frameHeight)
-video.set(10,130)
-
-while True:
-    success, img = video.read()
-    print(img)
-    cv2.imshow("Video", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# frameWidth = 640
+# frameHeight = 480
+# video = cv2.VideoCapture(0)
+# video.set(3,frameWidth)
+# video.set(4,frameHeight)
+# video.set(10,130)
+#
+# while True:
+#     success, img = video.read()
+#     print(img)
+#     cv2.imshow("Video", img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
--- chapter2.py
+++ chapter2.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
+# import matplotlib.pyplot as plt
 
 
 
 img = cv2.imread("Resources/news_i1.jpeg")
 kernel = np.ones((5,5), np.uint8)
+print(kernel)
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray, (7,7),0)
